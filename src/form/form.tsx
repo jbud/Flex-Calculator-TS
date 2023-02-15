30c93d06bad3f70cccc44b107d6b441804216552
jbud <jbudorg@gmail.com>
2023-02-15 02:35:17 -0800

    fix relative path bug & rebuild

--- src/form/form.tsx
+++ src/form/form.tsx
@@ -75,7 +75,7 @@ const Form = () => {
     };
 
     const getRunways = async (icao: string) => {
-        fetch('/database/runways/' + icao + '.json')
+        fetch('./database/runways/' + icao + '.json')
             .then((response) => response.json())
             .then((data) => {
                 const rws = data.runways;
