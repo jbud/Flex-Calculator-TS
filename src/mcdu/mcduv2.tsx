b15d49ead8181682359f354086dfa1acee0983e0
jbud <jbudorg@gmail.com>
2023-02-17 01:45:38 -0800

    Add MCDU Ver 2, SVG version of the MCDU for better scaling
    it is not yet ready for testing

---
+++ src/mcdu/mcduv2.tsx
@@ -0,0 +1,459 @@
+import React from 'react';
+
+function Mcduv2() {
+    return (
+        <>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 371 317"
+                width={371}
+                height={317}
+                style={{
+                    backgroundColor: '#000',
+                }}
+            >
+                <defs></defs>
+                <rect
+                    width={371}
+                    height={317}
+                    style={{
+                        stroke: 'rgb(0, 0, 0)',
+                    }}
+                    y={0}
+                    x={0}
+                    rx={8.213}
+                    ry={8.213}
+                />
+                <g>
+                    <text
+                        style={{
+                            fill: 'rgb(255, 255, 255)',
+                            fontFamily: 'HoneywellMCDU',
+                            fontSize: '22.7px',
+                            whiteSpace: 'pre',
+                        }}
+                        x={47.966}
+                        y={27.874}
+                    >
+                        {'TAKE OFF RWY'}
+                    </text>
+                    <text
+                        style={{
+                            fill: 'rgb(4, 180, 4)',
+                            fontFamily: 'HoneywellMCDU',
+                            fontSize: '17pt',
+                            whiteSpace: 'pre',
+                        }}
+                        x={171.927}
+                        y={4.463}
+                    >
+                        <tspan
+                            x={230.04}
+                            y={27.771}
+                            style={{
+                                fontSize: 23,
+                                wordSpacing: 0,
+                            }}
+                        >
+                            {'  31C'}
+                        </tspan>
+                    </text>
+                </g>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, -67.971497, 85.74012)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'V1'}
+                    </tspan>
+                    <tspan x={110.546} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, -69.446518, 131.065125)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'VR'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, -68.67099, 176.390121)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'V2'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(254, 154, 46)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={22.25}
+                    y={78.084}
+                >
+                    {'___'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(254, 154, 46)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={22.081}
+                    y={123.409}
+                >
+                    {'___'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(254, 154, 46)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={21.583}
+                    y={168.207}
+                >
+                    {'___'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    y={194.074}
+                    x={16.756}
+                >
+                    {'TRANS ALT'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, -93.878143, 267.017944)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'THR RED/ACC'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(92, 92, 92)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    x={28.98}
+                    y={284.711}
+                >
+                    {'UPLINK'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(92, 92, 92)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={15.661}
+                    y={304.96}
+                >
+                    {'<TO DATA'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(46, 204, 250)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={18.207}
+                    y={213.877}
+                >
+                    {'XXXX'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(46, 204, 250)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, -83.803467, 283.055389)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'XXXX/XXXX'}
+                    </tspan>
+                    <tspan x={110.546} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    y={58.144}
+                    x={133.159}
+                >
+                    {'FLP RETR'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={142.123}
+                    y={86.736}
+                >
+                    {'F=---'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    y={103.469}
+                    x={133.415}
+                >
+                    {'SLT RETR'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={142.379}
+                    y={132.061}
+                >
+                    {'S=---'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    y={148.635}
+                    x={147.6}
+                >
+                    {'CLEAN'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={142.177}
+                    y={177.321}
+                >
+                    {'O=---'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, 132.845673, 266.824982)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'ENG OUT ACC'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(46, 204, 250)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, 213.493759, 284.596161)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'XXXX'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, 121.344086, 221.525436)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'FLEX TO TEMP'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(46, 204, 250)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: 17,
+                        whiteSpace: 'pre',
+                    }}
+                    x={308.92}
+                    y={212.898}
+                >
+                    {'[  ]\xB0'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, 155.897385, 176.200424)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'FLAPS/THS'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(46, 204, 250)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: 17,
+                        whiteSpace: 'pre',
+                    }}
+                    x={296.682}
+                    y={167.166}
+                >
+                    {'[]/[ ]'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: '12pt',
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, 167.41777, 130.966248)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'TO SHIFT'}
+                    </tspan>
+                    <tspan x={110.5459976196289} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(92, 92, 92)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: 17,
+                        whiteSpace: 'pre',
+                    }}
+                    x={321.157}
+                    y={122.861}
+                >
+                    {'[ ]*'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(92, 92, 92)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    transform="matrix(1, 0, 0, 1, 176.643478, 150.242279)"
+                >
+                    <tspan x={110.546} y={-27.619}>
+                        {'[M]'}
+                    </tspan>
+                    <tspan x={110.546} dy="1em">
+                        {'\u200B'}
+                    </tspan>
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDUSmall',
+                        fontSize: 16,
+                        whiteSpace: 'pre',
+                    }}
+                    x={302.654}
+                    y={284.828}
+                >
+                    {'NEXT'}
+                </text>
+                <text
+                    style={{
+                        fill: 'rgb(255, 255, 255)',
+                        fontFamily: 'HoneywellMCDU',
+                        fontSize: '18.7px',
+                        whiteSpace: 'pre',
+                    }}
+                    x={289.335}
+                    y={305.077}
+                >
+                    {'PHASE>'}
+                </text>
+            </svg>
+        </>
+    );
+}
+
+export default Mcduv2;
